0562668db611b95c7248b56f39522fe61fc188de
envolution <involution@gmail.com>
2024-10-27 18:01:02 -0400

    i

--- zdbsp/version.sh
+++ zdbsp/version.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -s https://zdoom.org/files/utils/zdbsp/ | grep -o 'zdbsp[^"]*src\.zip' | sort -uV | tail -n1
+curl -s https://zdoom.org/files/utils/zdbsp/ | grep -o 'zdbsp[^"]*src\.zip' | sort -uV | tail -n1 | grep -oP '[0-9]+(\.[0-9]+)*'

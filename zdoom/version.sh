038e3875fd3ad7e88f163e3b4f2761ac23821c31
envolution <involution@gmail.com>
2024-10-27 17:59:02 -0400

    i

---
+++ zdoom/version.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sourcesite="https://zdoom.org/files/zdoom/"
+major=$(curl -s $sourcesite | grep -o 'title="[0-9]\+\.[0-9]\+"' | grep -o '[0-9]\+\.[0-9]\+' | sort -V | tail -n1)
+curl -s $sourcesite/$major/ | grep -o 'zdoom-[^"]*\-src' | sort -uV | tail -n1 | grep -oP '[0-9]+(\.[0-9]+)*'

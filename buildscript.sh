a610112e194fbade8c3ec7616e8ce2f3d15d0058
envolution <involution@gmail.com>
2024-10-27 15:50:54 -0400

    i

--- buildscript.sh
+++ buildscript.sh
@@ -47,7 +47,7 @@ fi
 if [ -f "${GITHUB_WORKSPACE}/${PKGBUILD_PATH}/version.sh" ]; then
     echo "Copying version file from ${PKGBUILD_PATH}"
     cp "${GITHUB_WORKSPACE}/${PKGBUILD_PATH}/version.sh" ./_PKGBUILD_version.sh
-    chmod 700 ./_PLGBUILD_version.sh
+    chmod 700 ./_PKGBUILD_version.sh
     NEW_VERSION=$(./_PKGBUILD_version.sh)
     echo "== Detected ${NEW_VERSION} from upstream, PKGBUILD updating... =="
     sed -i "s|pkgver=.*|pkgver=${NEW_VERSION}|" _PKGBUILD_version.sh

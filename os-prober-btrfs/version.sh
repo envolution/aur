3f09cb9157d53cdad440b9576c88b642759e168a
envolution <involution@gmail.com>
2024-10-27 20:15:03 -0400

    update

--- os-prober-btrfs/version.sh
+++ os-prober-btrfs/version.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#curl http://deb.debian.org/debian/pool/main/o/os-prober/ | grep -o 'os-prober_[^"]*.tar.xz' | sort -uV | tail -n1 | grep -oP '[0-9]+(\.[0-9]+)*'
-echo '1.79'
+curl http://deb.debian.org/debian/pool/main/o/os-prober/ | grep -o 'os-prober_[^"]*.tar.xz' | sort -uV | tail -n1 | grep -oP '[0-9]+(\.[0-9]+)*'

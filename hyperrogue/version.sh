d31f49f393ab8c961c1878c58a6b4671045e5e9e
envolution <involution@gmail.com>
2024-10-29 03:14:32 -0400

    update

--- hyperrogue/version.sh
+++ hyperrogue/version.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-curl https://api.github.com/repos/zenorogue/hyperrogue/tags | jq -r '.[1].name'
+#curl https://api.github.com/repos/zenorogue/hyperrogue/tags | jq -r '.[1].name'
+echo "v13.0r"

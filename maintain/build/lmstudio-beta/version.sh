6047d6253805f985aa2c1b1f7ceab325fc1f311b
envolution <involution@gmail.com>
2024-11-03 22:17:39 -0500

    aa

--- maintain/build/lmstudio-beta/version.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-curl -L https://lmstudio.ai/beta-releases |\
-     grep -o 'https://releases\.lmstudio\.ai/linux/x86/[0-9]\+.[0-9]\+.[0-9]\+/beta/[0-9]\+/LM_Studio-[0-9]\+.[0-9]\+.[0-9]\+.AppImage' |\
-     sed 's|.*/x86/\([0-9]\.[0-9]\.[0-9]\)/beta.*|\1|'

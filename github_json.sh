4b35bc57dcc7a73a62f6c795d16c2f941966b552
envolution <involution@gmail.com>
2024-11-06 17:34:54 -0500

    ii

--- github_json.sh
+++ github_json.sh
@@ -47,8 +47,16 @@ jq -s '.[0] * .[1]' workingver.json result.json > combined.json
 json_data=$(cat combined.json)
 
 # Use jq to iterate over each package and compare fields
+#echo "$json_data" | jq -r '
+#  .data | to_entries[] | 
+#  select(.value.version != .value.pkgbuildversion or .value.release != .value.pkgbuildrel) | 
+#  .key
+#'
 echo "$json_data" | jq -r '
-  .data | to_entries[] | 
-  select(.value.version != .value.pkgbuildversion or .value.release != .value.pkgbuildrel) | 
+  .data | to_entries[] |
+  select(
+    (.value | has("version") and has("pkgbuildversion") and has("release") and has("pkgbuildrel")) and
+    (.value.version != .value.pkgbuildversion or .value.release != .value.pkgbuildrel)
+  ) |
   .key
 '

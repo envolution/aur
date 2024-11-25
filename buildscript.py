0db138c0da341b36be067f9d6da879385f49de9b
envolution <involution@gmail.com>
2024-11-25 02:23:55 -0500

    aa

--- buildscript.py
+++ buildscript.py
@@ -168,7 +168,7 @@ class ArchPackageBuilder:
                 '--mflags', '--skipchecksums', '--mflags', '--skippgpcheck'
             ] + combined_dependencies
             
-            if self.run_subprocess(command):
+            if self.subprocess_runner(command):
                 return True, {"package_name": package_name, "message": f"Dependencies for package '{package_name}' installed successfully."}
             
             return False, {"package_name": package_name, "error": "Subprocess failed during dependency installation."}

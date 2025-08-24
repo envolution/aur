f4ec98a4eca68a0963cfb5d0907c283bc5ccf00e
involution <involution@gmail.com>
2025-08-24 03:59:53 -0400

    sync

--- maintain/build/chromium-widevine/_chrome_ver.sh
+++ maintain/build/chromium-widevine/_chrome_ver.sh
@@ -14,7 +14,7 @@ EOF
 _chrome_ver=$(nvchecker --logger json -c ./_chrome_ver.toml | jq -r '.version')
 rm ./_chrome_ver.toml
 if [[ ! $_chrome_ver =~ ^[0-9]+(\.[0-9]+)*$ ]]; then
-  echo "Error: chromium wideevine nvchecker returned invalid chrome version '$_chrome_ver'" >&2
+  echo "Error: chromium widevine nvchecker returned invalid chrome version '$_chrome_ver'" >&2
   exit 1
 fi
 sed -i "s/^_chrome_ver=.*/_chrome_ver=${_chrome_ver}/" PKGBUILD
